b58bc80336f2086cba9a376bbd7d9078048a44be
myanzik <myanz.470shrestha@gmail.com>
2024-11-10 16:27:24 +0700

    refactor firebase configuration

--- src/createDocuments.js
+++ src/createDocuments.js
@@ -1,17 +1,9 @@
 const { initializeApp } = require('firebase/app');
 const { getFirestore, collection, doc, setDoc } = require('firebase/firestore');
+const { firebaseConfig } = require('./constants/config');
 
 // Initialize Firebase
-const firebaseConfig = {
-  // Your Firebase config here
-  apiKey: "AIzaSyBM_MKsXkcXqA2eQWfdR_lp4KzZhDnrx54",
-  authDomain: "blockvocates.firebaseapp.com",
-  projectId: "blockvocates",
-  storageBucket: "blockvocates.appspot.com",
-  messagingSenderId: "508636376077",
-  appId: "1:508636376077:web:ecdcba2305ae7d8b7d7944",
-  measurementId: "G-YE646ZSKP9"
-};
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
